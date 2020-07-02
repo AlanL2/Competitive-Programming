fde5950774b3cb570ec94f09e5dd10ab340b04ec
Alan Li <63260793+AlanL2@users.noreply.github.com>
2020-07-01 20:48:40 -0400

    solving gt problems

---
+++ dmoj/ChainRule.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    public static void main(String[] args) throws IOException{
+        int n = readInt(), m = readInt();
+        ArrayList<edge> adj[] = new ArrayList[n+1];
+        for(int i = 0; i<=n; i++){
+            adj[i] = new ArrayList<>();
+        }
+        for(int i = 0; i<m; i++){
+            int a = readInt(), b = readInt(), w = readInt();
+            adj[a].add(new edge(b, w));
+            adj[b].add(new edge(a, w));
+        }
+        Queue<edge>Q = new LinkedList<>();
+        int[]dis = new int[n+1];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        dis[0] = 0;
+        Q.add(new edge(0, 0));
+        while(!Q.isEmpty()){
+            edge cur = Q.poll();
+            for(edge e:adj[cur.v]){
+                if(dis[e.v]>dis[cur.v]+e.w){
+                    dis[e.v] = dis[cur.v]+e.w;
+                    Q.add(new edge(e.v, dis[e.v]));
+                }
+            }
+        }
+        Q.add(new edge(n-1, 0));
+        int[]dis1 = new int[n+1];
+        Arrays.fill(dis1, Integer.MAX_VALUE);
+        dis1[n-1] = 0;
+        while(!Q.isEmpty()){
+            edge cur = Q.poll();
+            for(edge e:adj[cur.v]){
+                if(dis1[e.v]>dis1[cur.v]+e.w){
+                    dis1[e.v] = dis1[cur.v]+e.w;
+                    Q.add(new edge(e.v, dis1[e.v]));
+                }
+            }
+        }
+        int ans = -1;
+        for(int i = 0; i<=n; i++){
+            ans = Math.max(ans, dis[i]+dis1[i]);
+        }
+        System.out.println(ans);
+    }
+    static class edge{
+        int v, w;
+        edge(int v0, int w0){
+            v = v0;
+            w = w0;
+        }
+    }
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+                st = new StringTokenizer(input.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readChar () throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+        return input.readLine().trim();
+    }
+}
